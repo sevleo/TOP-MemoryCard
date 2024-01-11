93ad493212f6149da9333c3af1bd2dc15e4c25a7
Seva <seva.leonov@hotmail.com>
2024-01-11 18:04:50 +0700

    organize API and use Effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import CardGetter from "./components/CardGetter";
 import "./index.css";
 
 function App() {
+  const getPokemonsUrl = "https://pokeapi.co/api/v2/pokemon/?limit=649";
+
+  // Number of pokemons used in a single game
   const pokemonGameSetSize = 20;
 
   // All pokemons from API
@@ -11,13 +14,29 @@ function App() {
   // A subset of pokemons picked randomly for a given game
   const [pokemonGameSet, setPokemonGameSet] = useState([]);
 
-  const getPokemonsUrl = "https://pokeapi.co/api/v2/pokemon/?limit=50";
+  // A subset of pokemons with details
+  const [pokemonsDetails, setPokemonsDetails] = useState([]);
 
+  // Fetch all pokemons from the database
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const urlResponse = await fetch(getPokemonsUrl);
+        const pokemonData = await urlResponse.json();
+        setPokemons(pokemonData.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // When pokemons are fetched, select random pokemons for the game
   useEffect(() => {
     const pokemonGameSetIndices = [];
     while (pokemonGameSetIndices.length < pokemonGameSetSize) {
       const randomDecimal = Math.random();
-      const randomNumber = Math.floor(randomDecimal * 50);
+      const randomNumber = Math.floor(randomDecimal * 649);
       if (!pokemonGameSetIndices.includes(randomNumber)) {
         pokemonGameSetIndices.push(randomNumber);
       }
@@ -28,22 +47,52 @@ function App() {
     );
 
     setPokemonGameSet(newPokemonGameSet);
-    console.log(newPokemonGameSet);
   }, [pokemons]);
 
+  // When random pokemons are selected, fetch details about them
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const urlResponse = await fetch(getPokemonsUrl);
-        const pokemonData = await urlResponse.json();
-        console.log(pokemonData.results);
-        setPokemons(pokemonData.results);
-      } catch (error) {
-        console.error(error);
-      }
+    const fetchPokemonData = async (url) => {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
     };
+
+    // Fetch data
+    const fetchData = async () => {
+      const promises = [];
+      if (pokemonGameSet) {
+        pokemonGameSet.forEach((pokemon) => {
+          if (pokemon) {
+            promises.push(fetchPokemonData(pokemon.url));
+          }
+        });
+      }
+
+      const pokemonDataArray = await Promise.all(promises);
+
+      // Strip data
+      const newPokemonsDetails = [];
+      pokemonDataArray.forEach((pokemon) => {
+        const newPokemonDetails = {
+          name: "",
+          image: "",
+        };
+        newPokemonDetails.name = pokemon.name;
+        newPokemonDetails.image =
+          pokemon.sprites.other.dream_world.front_default;
+        newPokemonsDetails.push(newPokemonDetails);
+      });
+
+      setPokemonsDetails(newPokemonsDetails);
+    };
+
     fetchData();
-  }, []);
+  }, [pokemonGameSet]);
+
+  // When details about pokemons are available...
+  useEffect(() => {
+    console.log(pokemonsDetails);
+  }, [pokemonsDetails]);
 
   return (
     <>
